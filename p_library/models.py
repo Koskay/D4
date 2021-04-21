6f353cc6d4dc72a6a7453f4e7a917e0b11f7ca90
Kostya Spynaki <spynaki.k@yandex.ru>
2021-04-21 19:39:08 +0300

    'site'

---
+++ p_library/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.full_name
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey("Author", on_delete=models.CASCADE,
+                                related_name="book_author")
+    redaction = models.ForeignKey("Redaction", on_delete=models.CASCADE,null=True,blank=True,
+                                related_name="book_house")
+    copy_count = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+class Redaction(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
---
+++ p_library/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from p_library.models import Book
+from django.template import loader
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from p_library.models import Redaction
+
+# Create your views here.
+#def index(request):
+    #author = Author.objects.all()
+    #return render(request, 'index.html', context={'author':author})
+
+#def index(request):
+    #books = Book.objects.all()
+    #return render(request, 'index.html', context={'books':books})
+
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index')
+    else:
+        return redirect('/index')
+
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index')
+    else:
+        return redirect('/index')
+
+def redactions(request):
+    template = loader.get_template('redactions.html')
+    redactions = Redaction.objects.all()
+    data = {
+        "redactions": redactions,
+    }
+    return HttpResponse(template.render(data, request))
+
